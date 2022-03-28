bb723961daf433edb7c42a85a228feda5b405603
Ignacio Senestrari <senestrari25@gmail.com>
2022-03-28 19:25:21 -0300

    clase 3

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css"
+
+
+const NavBar = () => {
+    return (
+        <nav className="NavBar">
+            <p className="logo">E-commerce</p>
+            <p>Autos</p>
+            <p>Celulares</p>
+            <p>Auriculares</p>
+            <button className="log">Login</button>
+        </nav>
+    )
+
+}
+
+export default NavBar
